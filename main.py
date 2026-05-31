72d1fdf983301ddba6606e6a491c7578daf1e91f
Jivitesh Gudekar <gudekar.jivitesh@gmail.com>
2026-05-31 18:27:12 -0300

    added contextlibmanager and lifespan of fastapi session

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from docs_mcp import mcp as docs_mcp_server
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import contextlib
+
+@contextlib.contextmanager
+async def lifespan(app: FastAPI):
+    async with docs_mcp_server.session_manager():
+        yield
+
+app = FastAPI(lifespan=lifespan)
+
+# Allow CORS for all origins (for testing purposes)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
 
 def main():
     # This runs the server (stdio by default)
